97694eefb76d870e62c6414aba0c25807790f40e
Justin Williams <justinjohnwilliams@gmail.com>
2021-10-30 10:02:50 -0500

    003: html up and running

---
+++ 003/cmd/cyoaweb/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/justinjohnwilliams/cyoa"
+)
+
+func main() {
+	port := flag.Int("port", 9000, "port to serve on")
+	file := flag.String("file", "gopher.json", "the JSON file with the story")
+
+	flag.Parse()
+	fmt.Printf("using file %s\n", *file)
+
+	f, err := os.Open(*file)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.JSONStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	// %+v verbose print with field names
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
---
+++ 003/go.mod
@@ -0,0 +1,3 @@
+module github.com/justinjohnwilliams/cyoa
+
+go 1.16
