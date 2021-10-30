79eff2af68ccbaf854a9386067401856aa8c499f
Justin Williams <justinjohnwilliams@gmail.com>
2021-10-30 10:54:17 -0500

    003: more fun with functional options

--- 003/cmd/cyoaweb/main.go
+++ 003/cmd/cyoaweb/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"text/template"
 
 	"github.com/justinjohnwilliams/cyoa"
 )
@@ -27,7 +29,82 @@ func main() {
 		panic(err)
 	}
 
-	h := cyoa.NewHandler(story)
+	tpl := template.Must(template.New("").Parse(fooBarTemplate))
+
+	h := cyoa.NewHandler(story,
+		cyoa.WithTemplate(tpl),
+		cyoa.WithPathFunc(someOtherPathFn))
 	fmt.Printf("Starting the server on %d\n", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
+
+func someOtherPathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/foobar" || path == "/foobar/" {
+		path = "/foobar/intro"
+	}
+
+	return path[len("/foobar/"):]
+}
+
+var fooBarTemplate = `
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Choose Your Own Adventure</title>
+    </head>
+    <body>
+	<section class="page">
+        <h1>FOOBAR {{.Title}}</h1>
+        {{range .Paragraphs}}
+        <p>{{.}}</p>
+        {{end}}
+        <ul>
+            {{range .Options}}
+            <li><a href="foobar/{{.Chapter}}">{{.Text}}</a></li>
+            {{end}}
+        </ul>
+	</section>
+	    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FFFCF6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+    </body>
+</html>`
