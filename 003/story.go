d8abf0f65c96a5c26350a01e2f29cc581ea9c6d9
Justin Williams <justinjohnwilliams@gmail.com>
2021-10-31 08:40:28 -0500

    003: clean up

--- 003/story.go
+++ 003/story.go
@@ -5,77 +5,14 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strings"
 	"text/template"
 )
 
 func init() {
-	tpl = template.Must(template.New("").Parse(defaultTemplate))
+	tpl = template.Must(template.New("").Parse(DefaultTemplate))
 }
 
 var tpl *template.Template
-
-var defaultTemplate = `
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset="utf-8"/>
-        <title>Choose Your Own Adventure</title>
-    </head>
-    <body>
-	<section class="page">
-        <h1>{{.Title}}</h1>
-        {{range .Paragraphs}}
-        <p>{{.}}</p>
-        {{end}}
-        <ul>
-            {{range .Options}}
-            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-            {{end}}
-        </ul>
-	</section>
-	    <style>
-      body {
-        font-family: helvetica, arial;
-      }
-      h1 {
-        text-align:center;
-        position:relative;
-      }
-      .page {
-        width: 80%;
-        max-width: 500px;
-        margin: auto;
-        margin-top: 40px;
-        margin-bottom: 40px;
-        padding: 80px;
-        background: #FFFCF6;
-        border: 1px solid #eee;
-        box-shadow: 0 10px 6px -6px #777;
-      }
-      ul {
-        border-top: 1px dotted #ccc;
-        padding: 10px 0 0 0;
-        -webkit-padding-start: 0;
-      }
-      li {
-        padding-top: 10px;
-      }
-      a,
-      a:visited {
-        text-decoration: none;
-        color: #6295b5;
-      }
-      a:active,
-      a:hover {
-        color: #7792a2;
-      }
-      p {
-        text-indent: 1em;
-      }
-    </style>
-    </body>
-</html>`
 
 //READ: https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
 type HandlerOption func(h *handler)
@@ -86,14 +23,14 @@ func WithTemplate(t *template.Template) HandlerOption {
 	}
 }
 
-func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+func WithPathParserFunc(fn func(r *http.Request) string) HandlerOption {
 	return func(h *handler) {
-		h.pathFn = fn
+		h.pathParserFn = fn
 	}
 }
 
 func NewHandler(s Story, opts ...HandlerOption) http.Handler {
-	h := handler{s, tpl, defaultPathFn} // default options in handler
+	h := handler{s, tpl, DefaultPathParserFn} // default options in handler
 	for _, opt := range opts {
 		opt(&h)
 	}
@@ -101,22 +38,13 @@ func NewHandler(s Story, opts ...HandlerOption) http.Handler {
 }
 
 type handler struct {
-	s      Story
-	t      *template.Template
-	pathFn func(r *http.Request) string
-}
-
-func defaultPathFn(r *http.Request) string {
-	path := strings.TrimSpace(r.URL.Path)
-	if path == "/" || path == "" {
-		path = "/intro"
-	}
-
-	return path[1:]
+	s            Story
+	t            *template.Template
+	pathParserFn func(r *http.Request) string
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := h.pathFn(r)
+	path := h.pathParserFn(r)
 
 	if chapter, ok := h.s[path]; ok {
 		err := h.t.Execute(w, chapter)
